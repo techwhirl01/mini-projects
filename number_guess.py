4807fbb9262242a568d5acabf3e8c54b11503f92
suhani <135247653+techwhirl01@users.noreply.github.com>
2026-06-17 00:31:21 +0530

    Improve user prompts and feedback messages
    Here's the logic:
    
    Difference ≤ 5 → " Very close!"
    Difference ≤ 10 → "Close!"
    Difference ≤ 20 → "Getting warmer!"
    Otherwise → "Far away!"

--- number_guess.py
+++ number_guess.py
@@ -1,29 +1,45 @@
 import random
-#generate a random number
+
 num_to_guess = random.randint(1, 100)
-#loop
 attempts = 0
 
 while True:
     try:
-    #ask the user to make a guess
-        guess = int(input("guess the no. between 1 to 100: "))
-        #if no. is not in range.
+        guess = int(input("Guess the number between 1 and 100: "))
+
         if guess < 1 or guess > 100:
-            print ("please enter a number between 1 and 100.")
+            print("Please enter a number between 1 and 100.")
             continue
 
-        attempts +=1
-        
-        if guess < num_to_guess:
-            print("no. is too low!")
-        elif guess > num_to_guess:
-            print("no. is too high!")
-        else:
-            print(f"congo!, you got the right answer in {attempts} attempts.")
-            break
-        
-    except ValueError:
-        print("invalid input")
+        attempts += 1
+        difference = abs(num_to_guess - guess)
 
+        if guess < num_to_guess:
+            print("Number is too low!")
+            if difference <= 5:
+                print("Very close!")
+            elif difference <= 10:
+                print("Close!")
+            elif difference <= 20:
+                print("Getting warmer!")
+            else:
+                print("Far away!")
+
+        elif guess > num_to_guess:
+            print("Number is too high!")
+            if difference <= 5:
+                print("Very close!")
+            elif difference <= 10:
+                print("Close!")
+            elif difference <= 20:
+                print("Getting warmer!")
+            else:
+                print("Far away!")
+
+        else:
+            print(f"Congratulations! You got the right answer in {attempts} attempts.")
+            break
+
+    except ValueError:
+        print("Invalid input.")
 
